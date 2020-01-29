00935ebee562b1aed7124879861e19260b856f5d
Pooja <pooja@synclarity.in>
2020-01-29 12:25:37 +0530

    exception are added to show errror meassage

--- src/compress.php
+++ src/compress.php
@@ -2,53 +2,61 @@
     class Compress {
         function compress_image($image_path) 
         {
-            if  (in_array  ('curl', get_loaded_extensions())) 
+            try
             {
-                $ext = pathinfo($image_path, PATHINFO_EXTENSION);
+                if  (!in_array  ('curl', get_loaded_extensions()))  
+                {
+                    throw new Exception("CURL is not available on your web server! To optimize the image the CURL must be installed on your web server.");
+                }
+                $ext = pathinfo($image_path, PATHINFO_EXTENSION); 
                 $arr_file_types = ['png','jpg', 'jpeg'];
-                if (!(in_array($ext, $arr_file_types))) {
-                    die('Only image is allowed!');
+
+                try
+                {
+                    if (!(in_array($ext, $arr_file_types))) {
+                        throw new Exception("Only image is allowed!");
+                    }
+
+                    // optimize image using reSmush.it
+                    $file = $image_path;
+                    $mime = mime_content_type($file);
+                    $info = pathinfo($file);
+                    $name = $info['basename'];
+                    $output = new CURLFile($file, $mime, $name);
+                    $data = array(
+                        "files" => $output,
+                    );
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_URL, 'http://api.resmush.it/?qlty=80');
+                    curl_setopt($ch, CURLOPT_POST,1);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                    $result = curl_exec($ch);
+                    if (curl_errno($ch)) {
+                    $result = curl_error($ch);
+                    }
+                    curl_close ($ch);
+                
+                    $arr_result = json_decode($result);
+                
+                    // store the optimized version of the image
+                    $ch = curl_init($arr_result->dest);
+                    $fp = fopen($image_path, 'wb');
+                    curl_setopt($ch, CURLOPT_FILE, $fp);
+                    curl_setopt($ch, CURLOPT_HEADER, 0);
+                    curl_exec($ch);
+                    curl_close($ch);
+                    fclose($fp);
+                    echo "File uploaded successfully.";
                 }
-                // optimize image using reSmush.it
-                $file = $image_path;
-                $mime = mime_content_type($file);
-                $info = pathinfo($file);
-                $name = $info['basename'];
-                $output = new CURLFile($file, $mime, $name);
-                $data = array(
-                    "files" => $output,
-                );
-            
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, 'http://api.resmush.it/?qlty=80');
-                curl_setopt($ch, CURLOPT_POST,1);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-                $result = curl_exec($ch);
-                if (curl_errno($ch)) {
-                $result = curl_error($ch);
+                catch (Exception $e) {
+                    echo 'ErrorMessage: ' .$e->getMessage(), "\n";
                 }
-                curl_close ($ch);
-            
-                $arr_result = json_decode($result);
-            
-                // store the optimized version of the image
-                $ch = curl_init($arr_result->dest);
-                $fp = fopen($image_path, 'wb');
-                curl_setopt($ch, CURLOPT_FILE, $fp);
-                curl_setopt($ch, CURLOPT_HEADER, 0);
-                curl_exec($ch);
-                curl_close($ch);
-                fclose($fp);
-                echo "File uploaded successfully.";
-         
-            }  
-            else 
-            {
-                echo "CURL is not available on your web server! To optimize the image the CURL must be installed on your web server.";
             }
-            
+            catch (Exception $e) {
+                echo 'ErrorMessage: ' .$e->getMessage(), "\n";
+            }    
         }
     }
      
